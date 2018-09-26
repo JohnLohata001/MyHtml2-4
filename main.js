587b8e6e6a739b48106e8c28871273e550472647
JohnLohata001 <37670263+JohnLohata001@users.noreply.github.com>
2018-09-26 15:30:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// function toggleShowEl(el) {
+//     let myHide = document.getElementById('ulClass');
+//     if (el.className == 'toggle') {
+//         el.id = 'ulClass';
+//         myHide.style.display = 'block';
+//     } else {
+//         el.className = 'toggle';
+//         myHide.style.display = 'none';
+//     }
+// }
